da3e4468dd58758291ca8998a19a6f573694f5de
Casper da Costa-Luis <casper.dcl@physics.org>
2021-02-15 22:27:50 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ sudo apt install -y python3-tk >/dev/null 2>&1
 
 echo -e "\n--- INSTALLING PYTHON MODULES ---\n"
 pip3 install -U pip >/dev/null 2>&1
-python3 -m pip install numpy scipy scikit-image cloudpickle pillow PyInstaller ttkthemes pygame pyglm watchdog PyOpenGL PyOpenGL_accelerate >/dev/null 2>&1
 sudo apt install -y mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev
 sudo apt install -y mesa-utils libgl1-mesa-glx
 sudo apt install -y mesa-libGLw-dev.x86_64
 # sudo apt-get install -y libglu1-mesa-dev freeglut3-dev mesa-common-dev >/dev/null 2>&1
+python3 -m pip install -y /MathInspector >/dev/null 2>&1
 
 echo -e "\n--- COPYING STYLE FILES ---\n"
 sudo cp /MathInspector/mathinspector/style/arc.tcl /usr/local/lib/python3.6/dist-packages/ttkthemes/png/arc/
